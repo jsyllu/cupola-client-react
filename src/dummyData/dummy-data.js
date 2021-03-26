9402ff261b5453867a2c318fda395013f7e29ae0
jsyllu <jessicaly.beau@foxmail.com>
2021-03-26 02:00:22 -0400

    create dummy data of rent & sale properties

---
+++ src/dummyData/dummy-data.js
@@ -0,0 +1,11 @@
+export const DummyData = [
+    {rlid: 1, address: "boston,ma"},
+    {rlid: 2, address: "malden,ma"},
+    {rlid: 3, address: "allston,ma"},
+]
+
+export const SaleData = [
+    {slid: 1, address: "fenway,ma"},
+    {slid: 2, address: "cambridge,ma"},
+    {slid: 3, address: "east boston,ma"},
+]
