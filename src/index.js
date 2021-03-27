a30d61defb186f7e3dce64ae56cc0a9a31d076db
jsyllu <jessicaly.beau@foxmail.com>
2021-03-27 00:30:59 -0400

    update index - import jquery, popper.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'jquery'
+import "popper.js"
+import './index.css'
+import App from './App'
 
 ReactDOM.render(
     <App />,
