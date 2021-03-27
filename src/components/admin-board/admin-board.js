7fcc6361ea5ffbbe0438d1b28d4b792f75554bcb
jsyllu <jessicaly.beau@foxmail.com>
2021-03-27 00:27:21 -0400

    create admin board

---
+++ src/components/admin-board/admin-board.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AdminBoard = () => {
+    return (
+        <div className="admin-board container">
+            <h1>This is Admin Board</h1>
+        </div>
+    )
+}
+
+export default AdminBoard
