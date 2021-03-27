ff8321d187e52bc78fbdaaba25001ef7b3c7d5f7
jsyllu <jessicaly.beau@foxmail.com>
2021-03-27 00:39:28 -0400

    update property for sale

--- src/components/property/property-for-sale.js
+++ src/components/property/property-for-sale.js
@@ -1,28 +1,18 @@
-import React, {useEffect, useState} from "react"
-import {Helmet} from "react-helmet";
-import {SaleData} from "../../dummyData/dummy-data";
-import {useParams} from "react-router-dom";
+import React from "react"
+import {Helmet} from "react-helmet"
+import {useParams} from "react-router-dom"
 
 const PropertyForSale = ({p}) => {
     const {slid} = useParams()
-    const [property, setProperty] = useState({})
-
-    // function findById (id) {
-    //     return id === slid
-    // }
-    //
-    // useEffect(() => {
-    //     setProperty(SaleData.filter(findById))
-    //     console.log(property)
-    // }, [slid])
-
+    // const [property, setProperty] = useState({})
 
     return (
-        <div>
+        <div className="container">
             <Helmet>
                 <title>{p.address} for Sale | Cupola</title>
             </Helmet>
             <h1>Property for Sale</h1>
+            <p>SLID: {slid}</p>
             <p>Address: {p.address}</p>
         </div>
     )
