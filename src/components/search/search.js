8e220b170c4050eb18729abb1248a36b51a1f7bd
jsyllu <jessicaly.beau@foxmail.com>
2021-03-27 00:40:26 -0400

    update search template and style

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,45 +1,49 @@
 import React, {useState} from "react"
-import {Link} from "react-router-dom";
+import {Link} from "react-router-dom"
+import './search.style.client.css'
 
 const Search = () => {
     // search property types
-    const sale = "sale"
-    const rent = "rent"
+    const SALE = "sale"
+    const RENT = "rent"
 
-    const [type, setType] = useState(sale)
-    const [location, setLocation] = useState("Boston,MA")
+    const [type, setType] = useState(SALE)
+    const [searchInput, setSearchInput] = useState("Boston,MA")
 
     return (
         <div className="search container">
             <h1 className="search-heading">
                 Discover your new dream home
             </h1>
-            <div className="search-property-type">
-                <label htmlFor={sale}>
+            <div className="search-property-type radio-toggle">
+                <label htmlFor={SALE}>
                     Buy
                 </label>
                 <input className=""
+                       id={SALE}
                        type="radio"
-                       name="type"
-                       checked={type === sale}
-                       onClick={() => setType(sale)} />
-                <label htmlFor={rent}>
+                       name="type-radio"
+                       checked={type === SALE}
+                       onClick={() => setType(SALE)} />
+                <label htmlFor={RENT}>
                     Rent
                 </label>
                 <input className=""
+                       id={RENT}
                        type="radio"
-                       name="type"
-                       checked={type === rent}
-                       onClick={() => setType(rent)} />
+                       name="type-radio"
+                       checked={type === RENT}
+                       onClick={() => setType(RENT)} />
+                <div className="slide-item"></div>
             </div>
             <div className="search-property-location">
-                <div className="row">
+                <div className="search-bar row">
                     <input className=""
                            type="text"
                            placeholder="Enter address"
-                           value={location}
-                           onChange={(e) => setLocation(e.target.value)} />
-                    <Link to={`/${type}/${location}`}>
+                           value={searchInput}
+                           onChange={(e) => setSearchInput(e.target.value)} />
+                   <Link to={`/${type}/${searchInput}`}>
                         <button className="btn btn-outline-primary">
                             <i className="fas fa-search"></i>
                         </button>
