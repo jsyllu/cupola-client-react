8b7264e273c05e643eb033ba9958d6c5c8348471
jsyllu <jessicaly.beau@foxmail.com>
2021-03-29 12:59:57 -0400

    update footer & header style

--- src/common/header.js
+++ src/common/header.js
@@ -64,6 +64,16 @@ const Header = () => {
                     </div>
                 }
 
+                <label
+                    className="navbar-toggler"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </label>
+
                 <div className="navbar-menu collapse navbar-collapse"
                      id="navbarSupportedContent">
                     <ul className="navbar-nav">
