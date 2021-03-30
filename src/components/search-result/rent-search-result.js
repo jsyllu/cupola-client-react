2fc1ac14f78225e948372a45a1cccce04acccca3
jsyllu <32682105+jsyllu@users.noreply.github.com>
2021-03-30 00:45:41 -0400

    update search result - useEffect to find listings, use ListingCard component

--- src/components/search-result/rent-search-result.js
+++ src/components/search-result/rent-search-result.js
@@ -1,41 +1,61 @@
-import React from "react"
-import {Link, useParams} from "react-router-dom"
+import React, {useEffect} from "react"
+import {useParams} from "react-router-dom"
 import {Helmet} from "react-helmet"
 import {connect} from "react-redux"
+import propertyActions from "../actions/property-actions"
+import ListingCard from "./listing-card"
+import {PROPERTY_TYPE_RENT} from "../search/search-bar"
 
-const RentSearchResult = ({
-    rentals = []
-}) => {
+const RentSearchResult = (
+    {
+        rentalListings = [],
+        findRentalListings
+    }) => {
+
     const {location} = useParams()
+
+    useEffect(() => {
+        findRentalListings(location)
+    }, [location])
 
     return (
         <div className="container">
             <Helmet>
                 <title>Apartments for Rent in {location} | Cupola</title>
             </Helmet>
-            <h1>Search Result - Rental Listings</h1>
-            <ul>
-                {
-                    rentals.map((r, index) =>
-                        <Link to={`/rent/${location}/p/${r.zpid}`}
-                              key={index}>
-                            <li>
-                                {r["zpid"]} {/* test api*/}
-                                {/* {r.address} */}
-                            </li>
-                        </Link>
-                    )
-                }
-            </ul>
+            <h1>Rental Listings in {location}</h1>
+            <p>{rentalListings.length} results</p>
+            <div className="search-result-grid">
+                <div className="row">
+                    {
+                        rentalListings.map((r, index) =>
+                            <ListingCard listing={r}
+                                         type={PROPERTY_TYPE_RENT}
+                                         location={location}
+                                         gallery="https://photos.zillowstatic.com/fp/fd2fbce65c42716ce7a3b3b7dfe8643f-cc_ft_1536.jpg"
+                                         key={index} />
+                        )
+                    }
+                </div>
+            </div>
         </div>
     )
 }
-const stpm = (state) => {
-    return {
-        rentals : state.rentalListingReducer.rentalListing
+
+const stpm = (state) => ({
+    rentalListings: state.rentalListingReducer.rentalListings
+})
+
+const dtpm = (dispatch) => ({
+    findRentalListings: (location) => {
+        try {
+            propertyActions.findRentalListings(dispatch, {location})
+        } catch (e) {
+            alert(e.message)
+        }
     }
-}
+})
 
-const dtpm = (dispatch) => {}
-
-export default connect(stpm, dtpm)(RentSearchResult)
+export default connect
+(stpm, dtpm)
+(RentSearchResult)
--- src/components/search-result/sale-search-result.js
+++ src/components/search-result/sale-search-result.js
@@ -1,41 +1,61 @@
-import React from "react"
-import {Link, useParams} from "react-router-dom"
+import React, {useEffect} from "react"
+import {useParams} from "react-router-dom"
 import {Helmet} from "react-helmet"
 import {connect} from "react-redux"
+import propertyActions from "../actions/property-actions"
+import ListingCard from "./listing-card"
+import {PROPERTY_TYPE_SALE} from "../search/search-bar"
 
-const SaleSearchResult = ({
-    sales = []
-}) => {
+const SaleSearchResult = (
+    {
+        saleListings = [],
+        findSaleListings
+    }) => {
+
     const {location} = useParams()
+
+    useEffect(() => {
+        findSaleListings(location)
+    }, [location])
 
     return (
         <div className="container">
             <Helmet>
                 <title>Homes for Sale in {location} | Cupola</title>
             </Helmet>
-            <h1>Search Result - For Sale Listings</h1>
-            <ul>
-                {
-                    sales.map((s, index) =>
-                        <Link to={`/sale/${location}/p/${s.zpid}`}
-                              key={index}>
-                            <li>
-                                {s["zpid"]} {/* test api*/}
-                            </li>
-                        </Link>
-                    )
-                }
-            </ul>
+            <h1>Real Estate & Homes For Sale in {location}</h1>
+            <p>{saleListings.length} results</p>
+            <div className="search-result-grid">
+                <div className="row">
+                    {
+                        saleListings.map((s, index) =>
+                            <ListingCard listing={s}
+                                         type={PROPERTY_TYPE_SALE}
+                                         location={location}
+                                         gallery="https://i.ytimg.com/vi/bIONUutiutk/maxresdefault.jpg"
+                                         key={index} />
+                        )
+                    }
+                </div>
+            </div>
         </div>
     )
 }
 
-const stpm = (state) => {
-    return {
-        sales : state.saleListingReducer.saleListing
+const stpm = (state) => ({
+    saleListings: state.saleListingReducer.saleListings
+})
+
+const dtpm = (dispatch) => ({
+    findSaleListings: (location) => {
+        try {
+            propertyActions.findSaleListings(dispatch, {location})
+        } catch (e) {
+            alert(e.message)
+        }
     }
-}
+})
 
-const dtpm = (dispatch) => {}
-
-export default connect(stpm, dtpm)(SaleSearchResult)
+export default connect
+(stpm, dtpm)
+(SaleSearchResult)
