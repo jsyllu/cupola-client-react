bc0609d9ce45a92fbef2119e6f54d4379b8dedb3
jsyllu <32682105+jsyllu@users.noreply.github.com>
2021-03-31 23:18:15 -0400

    update sale & rental listing reducer - copy prevState

--- src/reducers/rental-listing-reducer.js
+++ src/reducers/rental-listing-reducer.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 
 import {
     FIND_RENTAL_LISTINGS
@@ -8,12 +8,13 @@ const initialState = {
     rentalListings : [
         // insert rental listings data
     ]
-};
+}
 
 const rentalListingReducer = (state = initialState, action) => {
     switch (action.type) {
         case FIND_RENTAL_LISTINGS:
             return {
+                ...state,
                 rentalListings : action.rentalListings
             }
         default:
--- src/reducers/sale-listing-reducer.js
+++ src/reducers/sale-listing-reducer.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 
 import {
     FIND_SALE_LISTINGS,
@@ -8,12 +8,13 @@ const initialState = {
     saleListings : [
         // insert sale listings data
     ]
-};
+}
 
 const saleListingReducer = (state = initialState, action) => {
     switch (action.type) {
         case FIND_SALE_LISTINGS:
             return {
+                ...state,
                 saleListings : action.saleListings
             }
         default:
