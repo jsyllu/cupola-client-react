587e52a88ee08b292992d1be8f68a94bdf5e22e5
jsyllu <32682105+jsyllu@users.noreply.github.com>
2021-04-20 00:11:45 -0400

    update buyer & tenant profile

--- src/components/profile/buyer-profile.js
+++ src/components/profile/buyer-profile.js
@@ -1,20 +1,66 @@
-import React from "react"
+import React, {useEffect} from "react"
+import userActions from '../actions/user-actions'
+import {connect} from 'react-redux'
+import ListingCard from '../search-result/listing-card'
+import {PROPERTY_TYPE_SALE} from '../../reducers/search-bar-reducer'
 
 const BuyerProfile = (
     {
-        openProfile
+        profile,
+        openProfile,
+        wishlist = [],
+        getBuyerWishlist,
+        updateBuyerWishlist
     }) => {
+
+    useEffect(() => {
+        if (openProfile && profile !== undefined) {
+            getBuyerWishlist(profile.wishList)
+        }
+    }, [openProfile])
+
+    const unlike = (listing) => {
+        wishlist.filter((l) => l !== listing)
+        updateBuyerWishlist(wishlist)
+    }
+
     return (
         <>
             {
                 openProfile &&
                 <div className="profile container">
-                    <h2>Buyer Profile</h2>
+                    <h2>Wish List of Listings for Sale</h2>
+                    <div className="search-result-grid">
+                        <div className="row">
+                            {
+                                wishlist.map((listing) => {
+                                    return (
+                                        <ListingCard listing={listing}
+                                                     type={PROPERTY_TYPE_SALE}
+                                                     gallery="https://i.ytimg.com/vi/bIONUutiutk/maxresdefault.jpg"
+                                                     star={true}
+                                                     unlike={unlike}
+                                                     key={listing._id} />
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
                 </div>
             }
         </>
     )
 }
 
+const stpm = (state) => ({
+    wishlist: state.userReducer.buyerWishlist
+})
 
-export default BuyerProfile
+const dtpm = (dispatch) => ({
+    getBuyerWishlist: (wishlist) => userActions.getBuyerWishlist(dispatch, wishlist),
+    updateBuyerWishlist: (wishlist) => userActions.updateBuyerWishlist(dispatch, wishlist)
+})
+
+export default connect
+(stpm, dtpm)
+(BuyerProfile)
--- src/components/profile/tenant-profile.js
+++ src/components/profile/tenant-profile.js
@@ -1,20 +1,66 @@
-import React from "react"
+import React, {useEffect} from "react"
+import userActions from '../actions/user-actions'
+import {connect} from 'react-redux'
+import ListingCard from '../search-result/listing-card'
+import {PROPERTY_TYPE_RENT} from '../../reducers/search-bar-reducer'
 
 const TenantProfile = (
     {
-        openProfile
+        profile,
+        openProfile,
+        wishlist = [],
+        getTenantWishlist,
+        updateTenantWishlist
     }) => {
+
+    useEffect(() => {
+        if (openProfile && profile !== undefined) {
+            getTenantWishlist(profile.wishList)
+        }
+    }, [openProfile])
+
+    const unlike = (listing) => {
+        wishlist.filter((l) => l !== listing)
+        updateTenantWishlist(wishlist)
+    }
+
     return (
         <>
             {
                 openProfile &&
                 <div className="profile container">
-                    <h2>Tenant Profile</h2>
+                    <h2>Wish List of Listings for Rent</h2>
+                    <div className="search-result-grid">
+                        <div className="row">
+                            {
+                                wishlist.map((listing) => {
+                                    return (
+                                        <ListingCard listing={listing}
+                                                     type={PROPERTY_TYPE_RENT}
+                                                     gallery="https://i.ytimg.com/vi/bIONUutiutk/maxresdefault.jpg"
+                                                     star={true}
+                                                     unlike={unlike}
+                                                     key={listing._id} />
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
                 </div>
             }
         </>
     )
 }
 
+const stpm = (state) => ({
+    wishlist: state.userReducer.tenantWishlist
+})
 
-export default TenantProfile
+const dtpm = (dispatch) => ({
+    getTenantWishlist: (wishlist) => userActions.getTenantWishlist(dispatch, wishlist),
+    updateTenantWishlist: (wishlist) => userActions.updateTenantWishlist(dispatch, wishlist)
+})
+
+export default connect
+(stpm, dtpm)
+(TenantProfile)
